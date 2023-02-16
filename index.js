fc36a26d174b8bee1bdeeb792733e2ecfe5d739c
przemyslawmatraj <Przemyslaw.Matraj@microsoft.wsei.edu.pl>
2023-02-17 00:02:40 +0100

    fix: cors

--- index.js
+++ index.js
@@ -6,21 +6,20 @@ const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
 
+server.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 server.use(jsonServer.bodyParser);
 server.use(middlewares);
 server.use(router);
 
 const PORT = 5555;
-
-server.use(
-  cors({
-    origin: true,
-    credentials: true,
-    preflightContinue: false,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  })
-);
-server.options("*", cors());
 
 server.listen(PORT, () => {
   console.log(`JSON Server is running on http://localhost:${PORT}`);
