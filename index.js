e2f3a437017e465f80c5d8ae28dcfe79ab9841fd
przemyslawmatraj <Przemyslaw.Matraj@microsoft.wsei.edu.pl>
2023-02-16 23:51:31 +0100

    fix: cors

--- index.js
+++ index.js
@@ -6,16 +6,21 @@ const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
 
-server.use(
-  cors({
-    origin: "https://product-campaigns-server-dmm1.vercel.app/",
-  })
-);
 server.use(jsonServer.bodyParser);
 server.use(middlewares);
 server.use(router);
 
 const PORT = 5555;
+
+server.use(
+  cors({
+    origin: true,
+    credentials: true,
+    preflightContinue: false,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
+server.options("*", cors());
 
 server.listen(PORT, () => {
   console.log(`JSON Server is running on http://localhost:${PORT}`);
